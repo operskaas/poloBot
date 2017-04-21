aca4a82b17b8e7716822583b43218b9a7489e2d5
Oskar Perskaas <oskar.perskaas@gmail.com>
2017-04-20 22:32:13 -0400

    try to connect with credentials

--- webSocket.js
+++ webSocket.js
@@ -1,30 +1,76 @@
-var autobahn = require('autobahn');
-var wsuri = "wss://api.poloniex.com";
-var connection = new autobahn.Connection({
-  url: wsuri,
+const autobahn = require('autobahn');
+const unirest = require('unirest');
+const jsSHA = require('jssha');
+const {createShaObj} = require('./sha.js');
+
+const connection = new autobahn.Connection({
+  url: "wss://api.poloniex.com",
   realm: "realm1"
 });
 
-var symbols = new Set();
+const symbols = new Set();
 
 connection.onopen = function (session) {
   // function marketEvent (args, kwargs) {
   //   console.log('MarketEvent args are: ' + args + ', kwargs are: ' + kwargs);
   // }
-  function tickerEvent (args, kwargs) {
-    const symbol = args[0];
+
+  function logNewSymbols(symbol) {
     if (!symbols.has(symbol)) {
       if (symbol.includes('ETH')) {
         console.log('**************ETH SYMBOL ****************');
       }
       console.log('new symbol: ' + symbol);
     }
-    symbols.add(args[0]);
-    // console.log('TickerEvent args are: ' + args[0] + ', kwargs are: ' + kwargs);
+    symbols.add(symbol);
   }
 
-  // session.subscribe('BTC_XMR', marketEvent);
+  function buy() {
+    unirest.post('https://poloniex.com/tradingApi')
+        .headers({'Key': 'the key', 'Sign': 'the sign'})
+        .send({
+          "parameter": "nonce",
+          "command": "buy",
+          "currencyPair": "BTC_ETH",
+          "rate": 3,
+          "amount": .0000001,
+        })
+        .end(function(response) {
+          console.log(response.body);
+        });
+  }
+  function getBalances() {
+
+    const body = JSON.stringify({
+      "command": "returnBalances",
+      "nonce": 4,
+    });
+    console.log(body);
+    const shaObj = createShaObj();
+    shaObj.update(body);
+    const sn = shaObj.getHMAC("HEX");
+    unirest.post('https://poloniex.com/tradingApi')
+        .type('json')
+        .headers({'Key': 'IHMONRUS-JR63BYK0-6582EE1V-OIBE3KBB', 'Sign': sn})
+        .send(body)
+        .end(function(response) {
+          console.log(response);
+        });
+  }
+
+  function tickerEvent (args, kwargs) {
+    const symbol = args[0];
+
+    if (symbol === 'BTC_ETH') {
+      const last = args[1];
+      const baseVol = args[5];
+      const quoteVol = args[6];
+      console.log(`BaseVol: ${baseVol}, quoteVol: ${quoteVol}, at ${last}`);
+    }
+  }
+
   session.subscribe('ticker', tickerEvent);
+  getBalances();
 };
 
 connection.onclose = function () {
